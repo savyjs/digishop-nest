2538ac24355a8506b9571d2950298eaaac1fd86e
savvyjs <savvyversa@gmail.com>
2022-08-07 15:28:38 +0430

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(
+      {
+        load: [databaseConfig],
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRoot(),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
