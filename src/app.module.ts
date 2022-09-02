091f8fd263229991e1163f7b8cbd335975b9d039
savy <savvyversa@gmail.com>
2022-09-03 00:01:18 +0430

    feat(TypeORM): use forRootAsync configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,40 @@
-import { Logger, LoggerService, Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import databaseConfig from "./config/database.config";
-import { DataSource } from "typeorm";
-import { parse } from "@typescript-eslint/parser";
+import { Logger, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 import { MarketModule } from './modules/market/market.module';
+import { HtppService } from './services/util/htpp/htpp.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(
       {
         load: [databaseConfig],
-        isGlobal: true
-      }
+        isGlobal: true,
+      },
     ),
-    TypeOrmModule.forRoot(),
-    MarketModule
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => (
+        {
+          type: 'postgres',
+          host: config.get('database.host'),
+          port: config.get('database.port'),
+          password: config.get('database.password'),
+          username: config.get('database.username'),
+          database: config.get('database.name'),
+        }
+      ),
+    }),
+    MarketModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, HtppService],
 })
 export class AppModule {
-  private logger = new Logger("Market.name");
-
-  constructor(dataSource: DataSource, config: ConfigService) {
-
-    let password: string = config.get("database.password");
-    this.logger.log("config:", config.get("database"));
-
-    console.info("config:", config.get("database"));
-    dataSource.setOptions({
-      type: "postgres",
-      host: config.get("database.host"),
-      port: config.get("database.port"),
-      password: password,
-      username: config.get("database.username"),
-      database: config.get("database.name")
-    }).initialize();
+  constructor() {
   }
-
 }
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
-  type: process.env.TYPE,
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  name: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT || 5432,
-}));
+export default registerAs('database', () => {
+
+  let options = {
+    type: process.env.TYPE,
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT || 5432,
+  };
+  console.log({ options });
+  return options;
+});
